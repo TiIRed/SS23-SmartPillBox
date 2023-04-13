42e75640ea062e01ef430c637f49a6db48e9cd86
Dwjacobs <dwjacobs123@gmail.com>
2023-04-13 17:13:49 -0500

    updated html files

--- HTML/Main.js
+++ HTML/Main.js
@@ -1,43 +1,103 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+function Click(button){
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    fullscreen: true,
-    frame: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	el = document.getElementById(button);
+	
+	if(button == "button1" && getTime() == mornTime()){
+		location.href = "./alert.html";
+	}
+	if(button == "button2" && getTime() == midTime()){
+		location.href = "./alert.html";
+	}
+	if(button == "button3" && getTime() == eveTime()){
+		location.href = "./alert.html";
+	}
+	
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('Setup.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+function getTime(){
+	let today = new Date();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+ 	let h = today.getHours();
+ 	let m = today.getMinutes();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+ 	return h+""+m;
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+}
+
+function mornTime(){
+	let hMorn = 10;
+	let mMorn = "36";
+	let time = formatTime(hMorn, mMorn);
+	
+	return time;
+
+}
+
+function midTime(){
+	let hMid = 10;
+	let mMid = "31";
+	let time = formatTime(hMid, mMid);
+	
+	return time;
+}
+
+function eveTime(){
+	let hEve = 10;
+	let mEve = "31";
+	let time = formatTime(hEve, mEve);
+	
+	return time;
+}
+
+
+function startTime(){
+	
+	const today = new Date();
+	let h = today.getHours();
+	let m = today.getMinutes();
+	let s = today.getSeconds();
+ 	m = checkTime(m);
+ 	s = checkTime(s);
+
+ 	if(getTime() == mornTime() || getTime() == midTime() || getTime() == eveTime()){
+
+ 		
+ 		
+ 	}
+
+ 	document.getElementById('head').innerHTML =  h + ":" + m + ":" + s;
+	setTimeout(startTime, 1000);
+}
+ 	
+ 
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function editTime(button, hours, mins){
+
+	b = document.getElementById(button).innerHTML = formatTime(hours, mins)
+
+}
+
+
+function formatTime(hours, mins){
+
+	if(hours > 12){
+		hours = hours-12;
+	}
+	
+	if(hours < 10){
+		hours = "0" + hours;
+	}
+	
+	if(mins < 10){
+		mins = "0" + mins;
+	}
+	
+	return hours+""+mins;
+
+}
